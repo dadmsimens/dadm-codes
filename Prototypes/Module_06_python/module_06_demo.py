8d57b6678c7993943d54564e2e39bb0263cac417
ishmur <mateuszpab@gmail.com>
2018-01-13 11:54:06 +0100

    Refactored to fully support Core module syntax
    - refactoring

--- Prototypes/Module_06_python/module_06_demo.py
+++ Prototypes/Module_06_python/module_06_demo.py
@@ -1,6 +1,8 @@
 import os
+import time
 
-from classes.mri_data import DiffusionData
+from inc import simens_dadm as smns
+from inc import module_06
 
 # Module parameters definition, chosen later using GUI
 SOLVERS = {
@@ -8,13 +10,13 @@ SOLVERS = {
     1: 'nls'
 }
 FIX_METHODS = {
-    0: 'zero',
-    1: 'abs',
-    2: 'cholesky'
+    0: 'abs',
+    1: 'cholesky'
 }
 
-# Workaround for not implemented modules
-DATASET_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '\\Module_06\\data\\'
+# Data location in ROOT
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+DATASETS_ROOT = PROJECT_ROOT + '\\Data\\Module_06_test\\'
 DATASETS = {
     0: 'rec_35.mat',
     1: 'rec_40.mat',
@@ -26,18 +28,16 @@ if __name__ == '__main__':
     # API parameters (passed from GUI)
     dataset_name = DATASETS[1]
     solver = SOLVERS[0]
-    fix_method = FIX_METHODS[1]
-    plotting = True  # True for verifying functionality
+    fix_method = FIX_METHODS[0]
 
-    # Module 01 - Reconstruction
-    dwi = DiffusionData(dataset_path=DATASET_ROOT + dataset_name)
+    # True to plot results
+    plotting = True
 
-    # Required preprocessing steps, currently a dummy method
-    dwi.preprocess()
-
-    # Module 08 - Skull stripping
-    dwi.strip_skull()
+    # Dummy function to preprocess (implements other modules functionality)
+    dwi = smns.mri_read_module_06(filename=DATASETS_ROOT + dataset_name)
 
     # Module 06 - Diffusion tensor estimation
-    # biomarkers is a dictionary of biomarkers: MD, RA, FA, VR, FA_rgb
-    dwi.get_dti_biomarkers(solver, fix_method, plotting)
+    # dwi.biomarkers is a dictionary of biomarkers: MD, RA, FA, VR, FA_rgb
+    time.perf_counter()
+    dwi = module_06.run_module(dwi, solver, fix_method, plotting)
+    print("Module 6 (DTI) time: {} seconds.\n".format(time.perf_counter()))
