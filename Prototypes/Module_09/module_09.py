588d46cda2f8392b7d0c0fd3bcfe804b214e2bfe
Magdalena Kucharska <magdalena.kucharskaa@gmail.com>
2018-01-15 02:11:21 +0100

    add GMM function

--- Prototypes/Module_09/module_09.py
+++ Prototypes/Module_09/module_09.py
@@ -68,9 +68,49 @@ imageHistogram = imHist(image)
 print(imageHistogram)
 
 """ Create Gauss mixture model function """
-def gmm(x,mu,v,p):
-    print(imageGmm)
+"""
+Matlab prototype
+function y = module9GaussDist(x,mu,v,p)
 
+% x - column vector with non-zeros elemnts of histogram
+% mu - column vector expected value of each clasters
+% v - column vector of variation
+% p - column vector of probability
+
+mu = mu(:);
+v = v(:);
+p = p(:);
+
+for i=1:size(mu,1)
+   differ = x-mu(i);
+   amplitude = p(i)/sqrt(2*pi*v(i)); 
+   y(:,i) = amplitude*exp(-0.5 * (differ.*differ)/v(i));
+end
+
+"""
+def gmm(x,mu,v,p):
+        
+    #x - column vector with non-zeros elemnts of histogram
+    #mu - column vector expected value of each clasters
+    #v - column vector of variation
+    #p - column vector of probability
+    
+    x = x.flatten()
+    mu = mu.flatten()
+    v = v.flatten()
+    p = p.flatten()
+    
+    y = []
+    
+    for i in range(mu.size):
+        differ=x-mu[i]
+        amplitude = p[i]/(math.sqrt(2*math.pi*v[i]))
+        app = amplitude*(np.exp((-0.5*(differ*differ))/v[i]))
+        y.append(app)
+        
+    y = np.transpose(y)
+    return(y)
+    
 """ Segmentation - main function """
 def segmentation(skullFreeImage):
     print(imageSegmentation)
