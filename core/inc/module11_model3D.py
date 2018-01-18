c69c4c5d127403cc96ac10acc213c0e841a9267f
kargaj <gajewskakarolina8@gmail.com>
2018-01-19 00:32:26 +0100

    Module11 - gui improved

--- core/inc/module11_model3D.py
+++ core/inc/module11_model3D.py
@@ -4,19 +4,14 @@ from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
 class model3D():
     def __init__(self, mri_data = None, frame = None, layout = None):
-        self.mri_data = mri_data
-        self.frame = frame
-        self.layout = layout
+        self.image, self.model = self.generate_model3D(mri_data)
+        self.iren, self.renderer = self.setup_render_window(frame, layout)
         self.clipped_model = None
-        self.image, self.model = self.generate_model3D()
-        self.setup_render_window()
 
-
-
-    def generate_model3D(self):
-        vtk_array = numpy_support.numpy_to_vtk(num_array=self.mri_data.transpose(2, 1, 0).ravel(),deep=True,array_type=vtk.VTK_FLOAT)
+    def generate_model3D(self, mri_data):
+        vtk_array = numpy_support.numpy_to_vtk(num_array=mri_data.transpose(2, 1, 0).ravel(),deep=True,array_type=vtk.VTK_FLOAT)
         image = vtk.vtkImageData()
-        image.SetDimensions(self.mri_data.shape)
+        image.SetDimensions(mri_data.shape)
         image.SetSpacing(1,1,1)
         image.GetPointData().SetScalars(vtk_array)
         #marching cubes
@@ -25,144 +20,78 @@ class model3D():
         model.SetValue(0,1)
         return image, model
 
-    def setup_render_window(self):
-        self.vtkWidget = QVTKRenderWindowInteractor(self.frame)
-        self.layout.addWidget(self.vtkWidget)
-        self.frame.setLayout(self.layout)
-        self.renderer = vtk.vtkRenderer()
-        self.renderer.SetBackground(0.2, 0.2,0.2)
-        self.vtkWidget.GetRenderWindow().AddRenderer(self.renderer)
-        self.iren = self.vtkWidget.GetRenderWindow().GetInteractor()
+    def setup_render_window(self, frame, layout):
+        vtkWidget = QVTKRenderWindowInteractor(frame)
+        layout.addWidget(vtkWidget)
+        renderer = vtk.vtkRenderer()
+        renderer.SetBackground(0.2, 0.2,0.2)
+        vtkWidget.GetRenderWindow().AddRenderer(renderer)
+        iren = vtkWidget.GetRenderWindow().GetInteractor()
         style = vtk.vtkInteractorStyleTrackballCamera()
-        self.iren.SetInteractorStyle(style)
-        self.preview_model()
-
-
-    def preview_model(self):
+        iren.SetInteractorStyle(style)
 
         mapper = vtk.vtkPolyDataMapper()
         mapper.SetInputConnection(self.model.GetOutputPort())
         mapper.ScalarVisibilityOff()
-        self.actor_clipped = vtk.vtkActor()
         self.actor = vtk.vtkActor()
         self.actor.SetMapper(mapper)
-        self.renderer.AddActor(self.actor)
-        self.renderer.ResetCamera()
-        cam1 = self.renderer.GetActiveCamera()
+        renderer.AddActor(self.actor)
+
+        renderer.ResetCamera()
+        cam1 = renderer.GetActiveCamera()
         cam1.Elevation(110)
         cam1.SetViewUp(0, 0, 1)
         cam1.Azimuth(45)
-        self.renderer.ResetCameraClippingRange()
+        renderer.ResetCameraClippingRange()
+        iren.Initialize()
+        return iren, renderer
+
+    def preview_model(self):
+        self.actor.VisibilityOn()
+        self.renderer.RemoveAllViewProps()
+        self.renderer.AddActor(self.actor)
         self.iren.Initialize()
 
-    def change_mode(self, mode):
-        if mode == 0:
-            self.renderer.RemoveActor(self.actor_clipped)
-            self.preview_model()
-        else:
-            self.renderer.RemoveActor(self.actor)
-            self.renderer.RemoveActor(self.actor_clipped)
 
-    def cut_model(self):
-        #self.renderer.AddActor(self.actor)
-        self.actor.VisibilityOn()
 
-        self.plane = vtk.vtkPlane()
+    def cut_model(self, plane_mode=False):
+        self.preview_model()
+
+        plane_widget = vtk.vtkImagePlaneWidget()
+        plane_widget.SetInteractor(self.iren)
+        #plane_widget.SetPlaceFactor(1.25)
+        plane_widget.SetInputData(self.image)
+        plane_widget.PlaceWidget()
+        plane_widget.SetPlaneOrientationToZAxes()
+
+        plane = vtk.vtkPlane()
         clipper = vtk.vtkClipPolyData()
         clipper.SetInputConnection(self.model.GetOutputPort())
-        clipper.SetClipFunction(self.plane)
+        clipper.SetClipFunction(plane)
         clipper.InsideOutOn()
-        clipMapper = vtk.vtkPolyDataMapper()
-        clipMapper.SetInputConnection(clipper.GetOutputPort())
-        clipMapper.ScalarVisibilityOff()
+        clip_mapper = vtk.vtkPolyDataMapper()
+        clip_mapper.SetInputConnection(clipper.GetOutputPort())
+        clip_mapper.ScalarVisibilityOff()
+        clip_actor = vtk.vtkActor()
+        clip_actor.SetMapper(clip_mapper)
 
-        self.clipActor = vtk.vtkActor()
-        self.clipActor.SetMapper(clipMapper)
-        self.clipActor.VisibilityOff()
-        #self.clipActor.GetProperty().SetOpacity(0.1)
+        if plane_mode:
+            plane_widget.TextureVisibilityOn()
+            clip_actor.GetProperty().SetOpacity(0.1)
+        else:
+            plane_widget.TextureVisibilityOff()
+        plane_widget.On()
 
-        self.clipActor.SetScale(1.01, 1.01, 1.01)
-        
-        plane = vtk.vtkPlane()
-        circleCutter = vtk.vtkCutter()
-        circleCutter.SetInputConnection(self.model.GetOutputPort())
 
-        circleCutter.SetCutFunction(self.plane)
-        stripper = vtk.vtkStripper()
-        stripper.SetInputConnection(circleCutter.GetOutputPort()) # valid circle
-        stripper.Update()
-        # that's our circle
-        circle = stripper.GetOutput()
-        
-        boundaryPoly = vtk.vtkPolyData()
-        boundaryPoly.SetPoints(circle.GetPoints())
-        boundaryPoly.SetPolys(circle.GetLines())
-        boundaryMapper = vtk.vtkPolyDataMapper()
-        boundaryMapper.SetInputData(boundaryPoly)
-        boundaryMapper.ScalarVisibilityOff()
-
-        
-        boundaryActor = vtk.vtkActor()
-
-        #boundaryActor.VisibilityOff()
         # The callback function
         def myCallback(obj, event):
-            boundaryActor.VisibilityOff()
-            self.plane.SetNormal(obj.GetNormal())
-            self.plane.SetOrigin(obj.GetCenter())
-            self.actor.VisibilityOff()
-            self.clipActor.VisibilityOn()
-            
-            
-        def myCallback2(obj, event):
-            plane = vtk.vtkPlane()
-            circleCutter = vtk.vtkCutter()
-            circleCutter.SetInputConnection(self.model.GetOutputPort())
-
-            circleCutter.SetCutFunction(self.plane)
-            stripper = vtk.vtkStripper()
-            stripper.SetInputConnection(circleCutter.GetOutputPort()) # valid circle
-            stripper.Update()
-            # that's our circle
-            circle = stripper.GetOutput()
-            
-            boundaryPoly = vtk.vtkPolyData()
-            boundaryPoly.SetPoints(circle.GetPoints())
-            boundaryPoly.SetPolys(circle.GetLines())
-            boundaryMapper = vtk.vtkPolyDataMapper()
-            boundaryMapper.SetInputData(boundaryPoly)
-            boundaryMapper.ScalarVisibilityOff()
-
-            
-            boundaryActor = vtk.vtkActor()
-
-            boundaryActor.SetMapper(boundaryMapper)
-            #boundaryActor.GetProperty().SetColor(0.8900,0.8100,0.3400)
             plane.SetNormal(obj.GetNormal())
             plane.SetOrigin(obj.GetCenter())
-            boundaryActor.VisibilityOn()
-            #boundaryActor.GetProperty().SetOpacity(0.5)
-            #self.renderer.AddActor(boundaryActor)
-            
-        planeWidget = vtk.vtkImagePlaneWidget()
-        planeWidget.SetInteractor(self.iren)
-        planeWidget.SetPlaceFactor(1.25)
-        planeWidget.SetInputData(self.image)
-        planeWidget.PlaceWidget()
-        planeWidget.On()
-        planeWidget.SetPlaneOrientationToZAxes()
-        
-        
-        planeWidget.AddObserver("InteractionEvent", myCallback)
-        planeWidget.AddObserver("EndInteractionEvent", myCallback2)
-        
-        planeWidget.TextureVisibilityOff()
-        
-        self.renderer.AddActor(self.actor)
-        self.renderer.AddActor(self.clipActor)
-        
-        
+            self.actor.VisibilityOff()
+
+        plane_widget.AddObserver("InteractionEvent", myCallback)
+
+        self.renderer.AddActor(clip_actor)
         self.iren.Initialize()
-        self.vtkWidget.GetRenderWindow().Render()
-        self.iren.Start()
+        #self.iren.Start()
 
