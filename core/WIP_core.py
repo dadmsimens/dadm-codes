26d63c6e3120eeeec7f24302601187bd2744fba7
LDTV <jacekffid@os.pl>
2018-01-22 16:16:25 +0100

    Heavy core and GUI development

--- core/WIP_core.py
+++ core/WIP_core.py
@@ -6,38 +6,58 @@ from inc.constants import *
 
 def simens_core(communicator):
 
-    state = 0;
-    
+    state = 0
+    data = []
+    current_instance = 0
 
     while True:
-        print("Core working")
+        #print("Core working")
         x=None
         if not communicator.gui_says.empty():
 
             x = communicator.gui_says.get()
 
             if x.module == READ_STR:
+                current_instance = 0
+                data = []
                 communicator.core_says.put('Opening data...')
-                mri_data = smns.mri_read(x.arguments)
+                data.append(smns.mri_read(x.arguments))
                 communicator.core_says.put('Reconstructing...')
-                mri_data = module_01.run_module(mri_data)
+                data[current_instance] = module_01.run_module(data[current_instance])
                 communicator.core_says.put('Reconstructing done')
-                communicator.core_says.put(smns.simens_msg('data', mri_data))
+                communicator.core_says.put(smns.simens_msg('data', data[current_instance]))
 
             elif x.module == MODULE_2_STR:
                 pass
 
             elif x.module == MODULE_3_STR:
-                pass
+                communicator.core_says.put('Explicit call for noise map is not supported')
 
             elif x.module == MODULE_4_STR:
-                pass
+                communicator.core_says.put('Generating noise maps...')
+                data[current_instance] = module3.main3(data[current_instance])
+                communicator.core_says.put('Filtering...')
+                data[current_instance] = module4.main4(data[current_instance])
+                communicator.core_says.put('Filtering done')
+                communicator.core_says.put(smns.simens_msg(MODULE_4_STR, data[current_instance]))
 
             elif x.module == MODULE_5_STR:
-                pass
+                communicator.core_says.put('Generating noise maps...')
+                data[current_instance] = module3.main3(data[current_instance])
+                communicator.core_says.put('Filtering...')
+                data[current_instance] = module_05.run_module(data[current_instance])
+                communicator.core_says.put('Filtering done')
+                communicator.core_says.put(smns.simens_msg(MODULE_5_STR, data[current_instance]))
 
             elif x.module == MODULE_6_STR:
-                pass
+                communicator.core_says.put('Obtaining DTI biomarkers...')
+                try:
+                    data[current_instance] = module_06.run_module(data[current_instance])
+                except ValueError:
+                    communicator.core_says.put('Module 6: Expected MRI_DIFF object instance, received unknown type.')
+                else:
+                    communicator.core_says.put('DTI complete')
+                    communicator.core_says.put(smns.simens_msg(MODULE_4_STR, data[current_instance]))
 
             elif x.module == MODULE_8_STR:
                 pass
@@ -49,10 +69,10 @@ def simens_core(communicator):
                 pass
 
             elif x.module == MODULE_11_STR:
-                pass
+                communicator.core_says.put('Brain 3D imaging takes place in GUI')
 
             elif x.module == MODULE_12_STR:
-                pass
+                communicator.core_says.put('Oblique imaging takes place in GUI')
 
             else:
                 communicator.core_says.put('Command not recognized!')
