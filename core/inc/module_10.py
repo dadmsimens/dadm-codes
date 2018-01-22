1cd3ec8050b529d449cb523ddaf94b55eff1193e
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2018-01-22 22:09:38 +0100

    function name changed

--- core/inc/module_10.py
+++ core/inc/module_10.py
@@ -19,26 +19,17 @@ import matplotlib.pyplot as plt
 from scipy import ndimage
 import math
 
-def initial_interp(image, N, M, display):
+def initial_interp(image, N, M):
     # initial interpolation
     interp_image = ndimage.zoom(image, N, mode='reflect')  # 512x512
-    # spline interpolation, mode is what to do with boundries - the same as next row/column
-    # size of interpolation, it has to be the same horizontally and vertically
-    if display == 1:
-        plt.imshow(image, cmap='gray')  # 256x256
-        plt.title('Original image')
-        plt.show()
-        plt.title('Spline interpolated image')
-        plt.imshow(interp_image, cmap='gray')
-        plt.show()
     return(interp_image)
 
 def Euclidean_dist(x1,y1,x2,y2):
     return(sqrt((x1-x2)**2+(y1-y2)**2))
 
-def upsampling(image, N, M, plotting):
+def module10(image, N):
     window=2;
-    image2 = initial_interp(image, N, M, 0);
+    image2 = initial_interp(image, N, N);
     s = image2.shape
 
     sigma = np.std(image2)
@@ -73,48 +64,33 @@ def upsampling(image, N, M, plotting):
             s1 = sum(wx*iw)
             s2 = sum(wx)
             filtered_image[i,j] = s1/s2
-
-    image3 = []
-
-    for i in np.arange(0,s[0],N):
-        for j in np.arange(0,s[1],M):
-            tmp = filtered_image[i:i+N-1,j:j+M-1]
-            offset = image[int((i+N-1)/N),int((j+M-1)/M)] - np.mean(tmp[:])
-            image3[i:int(i+N-1)][j:int(j+M-1)] = filtered_image[i:int(i+N-1)][j:int(j+M-1)] + offset
            
-
-    if plotting==True:
-        plt.imshow(image, cmap='gray')
-        plt.title('Original image')
-        plt.show()
-        plt.imshow(filtered_image, cmap='gray')
-        plt.title('Upsampled image')
-        plt.show()
+    return filtered_image
         
 
 
 from . import simens_dadm as smns
 
 def main10(mri_input, other_arguments = None):
-    if (isinstance(mri_input, smns.mri_diff)): # instructions for diffusion mri
+    if (isinstance(mri_input, smns.mri_diff)): 
         print("This file contains diffusion MRI")
         [m, n, slices, gradients] = mri_input.diffusion_data.shape
         data_out = np.zeros([m, n, slices, gradients])
    
         for i in range(slices):
             for j in range(gradients):
-                data_out[:, :, i, j] = upsampling(mri_input.diffusion_data[:, :, i, j])
+                data_out[:, :, i, j] = module10(mri_input.diffusion_data[:, :, i, j])
         	
         mri_input.diffusion_data = data_out
         
     
-    elif (isinstance(mri_input, smns.mri_struct)): # instructions specific for structural mri. The case of diffusion MRI is excluded here by elif.
+    elif (isinstance(mri_input, smns.mri_struct)): 
         print("This file contains structural MRI")
         [m, n, slices] = mri_input.structural_data.shape
         data_out = np.zeros([m, n, slices])
         
         for i in range(slices):
-            data_out[:, :, i] = upsampling(mri_input.structural_data[:, :, i], N, M, plotting)
+            data_out[:, :, i] = module10(mri_input.structural_data[:, :, i], N)
         	
         mri_input.structural_data = data_out
         
