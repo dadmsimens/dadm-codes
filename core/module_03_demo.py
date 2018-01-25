6f202f99d1dd1ffeb93712317477639ab7917704
Kacper <kacperturek@hotmail.com>
2018-01-25 01:07:39 +0100

    Module3 demo and data updated

--- core/module_03_demo.py
+++ core/module_03_demo.py
@@ -13,25 +13,32 @@ DATASETS = {
     1: 'noise',
 }
 
+
 if __name__ == "__main__":
     dataset_name = DATASETS[0]
     data = smns.load_object(file_path=DATASETS_ROOT + dataset_name)
-    data1 = module3.main3(data)
-    smns.save_object(file_path=DATASETS_ROOT+'noise', data_object=data1)
-		
-    dataset_name = DATASETS[1]
-    mri_data = smns.load_object(file_path=DATASETS_ROOT + dataset_name)
 
-    noisy = data.diffusion_data[:, :, 1, 10]
-    noise_map = mri_data.diff_noise_map[:, :, 1, 10]
+    estimate = 0
+    if estimate:
+        data1 = module3.main3(data)
+        smns.save_object(file_path=DATASETS_ROOT+'noise', data_object=data1)
 
-    fig = plt.figure()
-    plt.subplot(1, 2, 1)
-    plt.imshow(np.squeeze(noisy), cmap='gray')
-    plt.axis('off')
-    plt.title('noisy image')
-    plt.subplot(1, 2, 2)
-    plt.imshow(np.squeeze(noise_map))
-    plt.axis('off')
-    plt.title('noise map')
-    plt.show()
+    example = 1
+
+    if example:
+        dataset_name = DATASETS[1]
+        mri_data = smns.load_object(file_path=DATASETS_ROOT + dataset_name)
+
+        noisy = data.diffusion_data[:, :, 1, 10]
+        noise_map = mri_data.diff_noise_map[:, :, 1, 10]
+
+        fig = plt.figure()
+        plt.subplot(1, 2, 1)
+        plt.imshow(np.squeeze(noisy), cmap='gray')
+        plt.axis('off')
+        plt.title('Noisy image')
+        plt.subplot(1, 2, 2)
+        plt.imshow(np.squeeze(noise_map))
+        plt.axis('off')
+        plt.title('Noise map')
+        plt.show()
