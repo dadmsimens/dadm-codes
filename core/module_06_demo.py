189fa5897bb3e4cb70b55bb1f6e40b72e6225996
ishmur <mateuszpab@gmail.com>
2018-01-26 10:34:58 +0100

    Module 06 pipeline demo
    - added references to module 3, 5, 8

--- core/module_06_demo.py
+++ core/module_06_demo.py
@@ -2,7 +2,7 @@ import os
 import time
 
 from core.inc import simens_dadm as smns
-from core.inc import module_06
+from core.inc import module_06, module3, module_05, module08
 
 # Module parameters definition, chosen later using GUI
 SOLVERS = {
@@ -31,12 +31,21 @@ if __name__ == '__main__':
     # True to plot results
     plotting = True
     verbose = True
+    pipeline = True
 
-    # Dummy function to preprocess (implements other modules functionality)
+    # Load reconstructed data from file
     dwi = smns.load_object(file_path=DATASETS_ROOT+dataset_name)
 
+    # Pre-procesing pipeline
+    if pipeline is True:
+        time.perf_counter()
+        dwi = module3.main3(dwi)
+        dwi = module_05.run_module(dwi)
+        dwi = module08.main8(dwi, verbose=False)
+        dwi.skull_stripping_mask = dwi.skull_stripping_mask == 1
+        print("Module 3, 5, 8 (preprocessing) time: {} seconds.\n".format(time.perf_counter()))
+
     # Module 06 - Diffusion tensor estimation
-    # dwi.biomarkers is a dictionary of biomarkers: MD, RA, FA, VR, FA_rgb
     time.perf_counter()
     dwi = module_06.run_module(dwi, solver, fix_method, plotting, verbose)
     print("Module 6 (DTI) time: {} seconds.\n".format(time.perf_counter()))
